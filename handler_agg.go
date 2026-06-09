afe148e9fa65efb6d86677347d25728154a14b0a
GianImpedovo <impefranco98@gmail.com>
2026-06-09 18:06:54 +0000

    finalizo con el agg con tiempo

--- handler_agg.go
+++ handler_agg.go
@@ -2,16 +2,54 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
 )
 
 func handlerAgg(s *state, cmd command) error {
 
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+	if len(cmd.arguments) != 1 {
+		return errors.New("No arguments")
+	}
+
+	timeBetweenRequests, err := time.ParseDuration(cmd.arguments[0])
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("%+v\n", feed)
+	fmt.Printf("Collecting feeds every %v\n", timeBetweenRequests)
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+	for ; ; <-ticker.C {
+		err = scrapeFeeds(s)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+}
+
+func scrapeFeeds(s *state) error {
+
+	feed, err := s.db.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return err
+	}
+
+	err = s.db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		return err
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		return err
+	}
+
+	for _, v := range rssFeed.Channel.Item {
+		fmt.Printf("%s\n", v.Title)
+	}
+
 	return nil
 }
